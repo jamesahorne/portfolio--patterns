0708dc8577af14a491a231dfa4ee6ebd31f5435d
James Horne <james99h39@gmail.com>
2020-03-30 17:59:22 +0100

    ♻️ Refactor accordion

--- source/js/main.js
+++ source/js/main.js
@@ -1,25 +1,25 @@
 const allAccordions = document.getElementsByClassName("c-accordion__heading");
-const activeAccordions = document.getElementsByClassName("c-accordion__heading isActive");
+const activeAccordions = document.getElementsByClassName("c-accordion__heading c-accordion__heading--is-active");
 
 // Open first accordion
 const firstAccordion = allAccordions[0];
 const firstPanel = firstAccordion.nextElementSibling;
-firstAccordion.classList.add('isActive');
+firstAccordion.classList.add('c-accordion__heading--is-active');
 firstPanel.style.maxHeight = firstPanel.scrollHeight + 'px';
 
 // Loop through all accordions in allAccordions to add a click event listener
 for (let i = 0; i < allAccordions.length; i++) {
   allAccordions[i].addEventListener("click", function() {
-    let isActive = this.classList.contains('isActive');
+    let isActiveAccordion = this.classList.contains('c-accordion__heading--is-active');
 
     // If it's already open, leave it.
     // Otherwise, shut all other accordions and open the one clicked on
-    if (!isActive) {
+    if (!isActiveAccordion) {
 
-      // Loop through activeAccordions and remove isActive class and panel height
+      // Loop through activeAccordions and remove active class and panel height
       for (let j = 0; j < activeAccordions.length; j++) {
         let currentAccordion = activeAccordions[j];
-        currentAccordion.classList.remove('isActive');
+        currentAccordion.classList.remove('c-accordion__heading--is-active');
         let panel = currentAccordion.nextElementSibling;
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
@@ -28,8 +28,8 @@ for (let i = 0; i < allAccordions.length; i++) {
         }
       }
 
-      // Add isActive class and panel height
-      this.classList.toggle('isActive');
+      // Add active class and panel height
+      this.classList.toggle('c-accordion__heading--is-active');
       let panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
