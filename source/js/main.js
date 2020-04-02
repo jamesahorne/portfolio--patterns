1a16cddbdeda77df54c9a338f3c31e3d1233eec4
James Horne <james99h39@gmail.com>
2020-04-02 12:18:59 +0100

    ♻️ Style border and correct its height

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,18 @@
 // Accordion
-const allAccordions = document.getElementsByClassName("c-accordion__heading");
-const activeAccordions = document.getElementsByClassName("c-accordion__heading c-accordion__heading--is-active");
+const allAccordions = document.getElementsByClassName('c-accordion__heading');
+const activeAccordions = document.getElementsByClassName('c-accordion__heading c-accordion__heading--is-active');
 const firstAccordion = allAccordions[0];
 const accordionHeight = firstAccordion.scrollHeight;
 
 for (let i = 0; i < allAccordions.length; i++) {
-  allAccordions[i].addEventListener("click", function() {
+  allAccordions[i].addEventListener('click', function() {
     let isActiveAccordion = this.classList.contains('c-accordion__heading--is-active');
     let accordionSection = this.parentNode;
     let panel = this.nextElementSibling;
     let prevAccordion = activeAccordions[0] || firstAccordion;
     let prevAccordionSection = prevAccordion.parentNode;
     let prevPanel = prevAccordion.nextElementSibling;
+    const accordionBorders = document.getElementsByClassName('c-accordion__border');
 
     // If active, close it, else, first close previous accordion and then open current one
     if (isActiveAccordion) {
@@ -27,6 +28,10 @@ for (let i = 0; i < allAccordions.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + 24 + 'px';
       accordionSection.style.height = panel.scrollHeight + accordionHeight + 'px';
     }
+
+    for (var border of accordionBorders) {
+      border.style.height = accordionHeight - 32 + 'px';
+    }
   });
 }
 
@@ -36,5 +41,5 @@ for (let i = 0; i < allAccordions.length; i++) {
 
 // TODO
 // 
-// Span |
+// Box shadow
 // Accessibility
