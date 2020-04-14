73c079cc6ddf216fb716d533a28daa6c4d9c3e63
James Horne <james99h39@gmail.com>
2020-04-14 11:24:43 +0100

    ♻️ Remove component and group namespacing

--- source/js/main.js
+++ source/js/main.js
@@ -1,37 +1,37 @@
 //            Accordion            //
 
-const accordionComponent = document.getElementById('c-accordion');
+const accordionComponent = document.getElementById('accordion');
 
 // If it doesn't exist you get a console error (if it isn't "undefined" and it isn't "null", then it exists)
 if (typeof(accordionComponent) != 'undefined' && accordionComponent != null) {
-  const allAccordions = document.getElementsByClassName('c-accordion__button');
-  const activeAccordions = document.getElementsByClassName('c-accordion__button c-accordion__button--is-active');
+  const allAccordions = document.getElementsByClassName('accordion__button');
+  const activeAccordions = document.getElementsByClassName('accordion__button accordion__button--is-active');
   const firstAccordion = allAccordions[0];
   const accordionHeight = firstAccordion.scrollHeight;
 
   for (let i = 0; i < allAccordions.length; i++) {
     allAccordions[i].addEventListener('click', function() {
-      let isActiveAccordion = this.classList.contains('c-accordion__button--is-active');
+      let isActiveAccordion = this.classList.contains('accordion__button--is-active');
       let accordionSection = this.parentNode.parentNode;
       let panel = this.parentNode.nextElementSibling;
       let prevAccordion = activeAccordions[0] || firstAccordion;
       let prevAccordionSection = prevAccordion.parentNode.parentNode;
       let prevPanel = prevAccordion.parentNode.nextElementSibling;
-      const accordionBorders = document.getElementsByClassName('c-accordion__border');
+      const accordionBorders = document.getElementsByClassName('accordion__border');
 
       // If active, close it, else, first close previous accordion and then open current one
       if (isActiveAccordion) {
-        prevAccordion.classList.remove('c-accordion__button--is-active');
+        prevAccordion.classList.remove('accordion__button--is-active');
         prevAccordion.setAttribute('aria-expanded', 'false');
         prevPanel.style.maxHeight = null;
         prevAccordionSection.style.height = accordionHeight + 'px';
       } else {
-        prevAccordion.classList.remove('c-accordion__button--is-active');
+        prevAccordion.classList.remove('accordion__button--is-active');
         prevPanel.style.maxHeight = null;
         prevAccordionSection.style.height = accordionHeight + 'px';
         prevAccordion.setAttribute('aria-expanded', 'false');
 
-        this.classList.toggle('c-accordion__button--is-active');
+        this.classList.toggle('accordion__button--is-active');
         this.setAttribute('aria-expanded', 'true');
         panel.style.maxHeight = panel.scrollHeight + 24 + 'px';
         accordionSection.style.height = panel.scrollHeight + accordionHeight + 'px';
